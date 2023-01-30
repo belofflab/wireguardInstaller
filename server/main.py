8c853aee7aa9bbd30f8cbbe0627bb83045d84329
Константин Белов <russianosint@gmail.com>
2023-01-30 01:39:03 -0700

    fastapi add

--- server/main.py
+++ server/main.py
@@ -9,10 +9,10 @@ def clean_iptables(address: str ):
   iptables_address = subprocess.check_output('docker exec -t wireguard iptables -L INPUT --line-number', shell=True).decode('utf-8').split('\n')
   matches = [iptable for iptable in iptables_address if address in iptable]
   if len(matches) > 1:
-    subprocess.check_output(f'docker exec -t wireguard iptables -D INPUT {matches[0].split(" ")[0]}')
+    subprocess.check_output(f'docker exec -t wireguard iptables -D INPUT {matches[0].split(" ")[0]}', shell=True)
   else:
     for match in matches:
-      subprocess.check_output(f'docker exec -t wireguard iptables -D INPUT {match.split(" ")[0]}')
+      subprocess.check_output(f'docker exec -t wireguard iptables -D INPUT {match.split(" ")[0]}', shell=True)
 
 
 @app.get('/address/block')

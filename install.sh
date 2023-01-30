860ce433c1f50c990e45f539f70354fb8c51c984
Константин Белов <russianosint@gmail.com>
2023-01-30 00:19:50 -0700

    fastapi add

--- install.sh
+++ install.sh
@@ -10,4 +10,17 @@ sudo apt install docker-ce -y
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-cd /home/wireguard-up/ && docker-compose up -d 
+# cd /home/wireguard-up/ && docker-compose up -d 
+docker run -d \
+--sysctl net.ipv6.conf.all.disable_ipv6=0 \
+--cap-add NET_ADMIN \
+--name wg-manager \
+#--net host \
+-p "51800-51900:51800-51900/udp" \
+-p "8888:8888" \
+-v wg-manager:/config \
+-e HOST="0.0.0.0" \
+-e PORT="8888" \
+-e ADMIN_USERNAME="admin" \
+-e ADMIN_PASSWORD="admin" \
+perara/wg-manager

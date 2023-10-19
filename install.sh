125d259f38fcc6cf7d6c6493358e3be8167d6712
belofflab <83600812+belofflab@users.noreply.github.com>
2023-10-19 07:24:04 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-cache policy docker-ce
 sudo apt install docker-ce -y
 
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 cd /home/wireguard-up/ && docker-compose up -d 
